3b9d1db77e5507d4b0466ba446005bb3a2f59a2e
Ko, Ji han <JI_HAN_KO@homedepot.com>
2020-12-28 15:16:32 -0500

    figured out which group has odd weighted islander

--- RiddleRunner.java
+++ RiddleRunner.java
@@ -1,6 +1,6 @@
 public class RiddleRunner {
     public static void main(String[] args) {
-        TwelveIslander twelveIslander = new TwelveIslander(1);
+        TwelveIslander twelveIslander = new TwelveIslander(5);
         twelveIslander.runRiddle();
     }
 }
--- TwelveIslanderSolver.java
+++ TwelveIslanderSolver.java
@@ -34,7 +34,7 @@ public class TwelveIslanderSolver {
         System.out.println("////////////////////////////\n");
         System.out.println("Mapping aliases to respective weights");
         divideIntoThreeGroups();
-        firstEvaluation();
+        evaluation();
     }
 
     // private void mapAliasesToWeights(int[] islanders) {
@@ -62,8 +62,37 @@ public class TwelveIslanderSolver {
         }
     }
 
-    private boolean firstEvaluation() {
+    private int evaluation() {
         System.out.println("***First Evaluation***");
-        return false;
+        System.out.println("\nWeighing group 1 and 2...");
+        int seesawResult = this.seesaw.weigh(threeGroups[0], threeGroups[1]);
+        if (seesawResult > 0) {
+            System.out.println("Group 1 is heavier than Group 2!!!");
+            return firstInequalityEvaluation(seesawResult);
+        } else if (seesawResult < 0) {
+            System.out.println("Group 1 is lighter than Group 2!!!");
+            return firstInequalityEvaluation(seesawResult);
+        } else {
+            System.out.println("Group 1 and Group 2 weigh the same!!!");
+            System.out.println("Group 3 contains the odd weighted islander!!!");
+        }
+        System.out.printf("\nRemaining seesaw attempts: %d", this.seesaw.getAttempts());
+        return equalityEvaluation();
+    }
+
+    private int firstInequalityEvaluation(int seesawResult) {
+        System.out.println("***SECOND EVALUATION***");
+        System.out.println("\nWeighing Group 1 and 3...");
+        int newSeesawResult = this.seesaw.weigh(threeGroups[0], threeGroups[2]);
+        if (seesawResult == newSeesawResult) {
+            System.out.println("Group 1 contains the odd weighted islander!!!");
+        } else {
+            System.out.println("Group 2 contains the odd weighted islander!!!");
+        }
+        return 0;
+    }
+
+    private int equalityEvaluation() {
+        return 0;
     }
 }
